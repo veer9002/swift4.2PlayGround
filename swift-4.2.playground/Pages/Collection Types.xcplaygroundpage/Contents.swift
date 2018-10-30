31ed99777296d8b60d93888f1d68761fd2cd5c15
Manish Sharma <manish27698@gmail.com>
2018-10-30 17:12:58 +0530

    Collection Types added

---
+++ swift-4.2.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+
+import Foundation
+
+/*
+    Collection type in swift are Array, dictionary and sets
+ */
+
+
+// Array
+
+// Creating an empty array
+var emptyArray = [Int]()
+print("Count of empty array are \(emptyArray.count)")
+
+// append emptyArrat
+emptyArray.append(2)
+print("Count of empty array are \(emptyArray.count)")
+
+// delete the object from array and empty it again
+emptyArray = []
+print("Count of empty array are \(emptyArray.count)")
+
+
+// repeating array with default values
+var defaultArray = Array(repeating: 3.5, count: 3)
+
+
+
+// Dictionary
+
+
+
+// Set
