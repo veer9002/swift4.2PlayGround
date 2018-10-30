499261a30663147c896c7eefab34197d961603f4
Manish Sharma <manish27698@gmail.com>
2018-10-30 18:32:15 +0530

    Added some control flow

--- swift-4.2.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
+++ swift-4.2.playground/Pages/Collection Types.xcplaygroundpage/Contents.swift
@@ -6,7 +6,8 @@ import Foundation
  */
 
 
-// Array
+//-------------------- Array -------------------------//
+// Array is ordered collection of data
 
 // Creating an empty array
 var emptyArray = [Int]()
@@ -20,14 +21,66 @@ print("Count of empty array are \(emptyArray.count)")
 emptyArray = []
 print("Count of empty array are \(emptyArray.count)")
 
-
 // repeating array with default values
 var defaultArray = Array(repeating: 3.5, count: 3)
 
+// isEmpty short cut replacing arrayname.count
+if emptyArray.isEmpty {
+    print("Empty array")
+} else {
+    print("Object is available")
+}
+
+// adding new items into the array
+emptyArray += [23,25,23,45]
+print("Total count of empty array \(emptyArray.count)")
+
+// Iterating over an array
+for item in emptyArray {
+    print(item)
+}
+
+// use enumerate for index of each item
+for (item,value) in emptyArray.enumerated() {
+    print("Value at index \(item) is \(value)")
+}
+
+//------------------ End Array -------------------------//
 
 
-// Dictionary
+
+//------------------ Dictionary -------------------------//
+// A dictionary stores associations between keys of the same type and values of the same type in a collection with no defined ordering
+
+// empty dictionary
+var intValues = [Int: String]()
+
+// adding value
+intValues[16] = "Sixteen"
+print("Total dictionary object \(intValues.count)")
+
+//empty it again
+intValues = [:]
+print("Total dictionary object \(intValues.count)")
+
+// All the remaining part is similar to arrays and set
+
+//------------------ End Dictionary -------------------------//
 
 
 
-// Set
+//--------------------- Sets -------------------------//
+// Unordered colletion of data is set
+
+// initialising an empty set
+var emtpySet = Set<Character>()
+print("letter in emptySet are \(emtpySet.count)")
+
+// insert
+emtpySet.insert("d")
+print("letter count \(emtpySet.count)")
+
+// remove all and become empty
+emtpySet = []
+
+//-------------------- End Sets -------------------------//
--- swift-4.2.playground/Pages/Control-Flow.xcplaygroundpage/Contents.swift
+++ swift-4.2.playground/Pages/Control-Flow.xcplaygroundpage/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 
-var str = "Hello, swift"
+// For-In Loops
+let names = ["name1","name2","name3","name4","name5"]
+for name in names {
+    print("Name is \(name)")
+}
 
+let numberOfLegs = ["Spider": 8, "Aunt": 6, "Money": 2]
+for (key,value) in numberOfLegs {
+    print("\(key) has \(value) legs")
+}
 
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base
+}
